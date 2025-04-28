88947ea1300c5db3ce238c06b8549ba08721caa7
Essam <essamhamdy198@gmail.com>
2025-04-29 01:41:23 +0300

    Splash View ui done

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/helpers/on_generate_route.dart';
+import 'package:fruits_hub/features/splash/presentation/views/splash_view.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const FruitHub());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class FruitHub extends StatelessWidget {
+  const FruitHub({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+      onGenerateRoute: onGenerateRoute,
+      initialRoute: SplashView.routeName,
+      home: Scaffold(body: SplashView()),
     );
   }
 }
