1ce4c32d1db03f62fb25483b8bc3c12d65ef3d7e
Essam <essamhamdy198@gmail.com>
2025-04-29 01:54:50 +0300

    splash view logic done

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fruits_hub/core/utils/app_images.dart';
+import 'package:fruits_hub/features/on_boarding_view/presentation/view/on_boarding_view.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    excuteNavigation();
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,5 +34,11 @@ class SplashViewBody extends StatelessWidget {
         SvgPicture.asset(Assets.imagesSplashBottom, fit: BoxFit.fill),
       ],
     );
+  }
+
+  void excuteNavigation() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+    });
   }
 }
