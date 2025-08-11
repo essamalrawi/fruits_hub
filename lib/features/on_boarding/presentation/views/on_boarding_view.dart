8f333b96333bd74cfdc48f3c3d8343e4dd1e2c84
Essam Al-Rawi <essamhamdy198@gmail.com>
2025-08-11 03:22:50 +0300

    fix names for constants

--- lib/features/on_boarding/presentation/views/on_boarding_view.dart
+++ lib/features/on_boarding/presentation/views/on_boarding_view.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:fruits_hub/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart';
+
+import 'widgets/on_boarding_view_body.dart';
 
 class OnBoardingView extends StatelessWidget {
   const OnBoardingView({super.key});
-  static const String routeName = "onBoarding";
+  static const String routeName = 'onBoardingView';
   @override
   Widget build(BuildContext context) {
-    return OnBoardingViewBody();
+    return const Scaffold(
+      body: SafeArea(child: OnBoardingViewBody()),
+    );
   }
 }
