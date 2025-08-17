dc82cd92ec6c9b8254dd15a88899fa95b7462c3e
Essam Al-Rawi <essamhamdy198@gmail.com>
2025-08-17 16:55:24 +0300

    add firebase

--- lib/features/auth/presentation/views/widgets/have_an_account_widget.dart
+++ lib/features/auth/presentation/views/widgets/have_an_account_widget.dart
@@ -3,12 +3,9 @@ import 'package:flutter/material.dart';
 
 import '../../../../../core/utils/app_colors.dart';
 import '../../../../../core/utils/app_text_styles.dart';
-import '../signup_view.dart';
 
 class HaveAnAccountWidget extends StatelessWidget {
-  const HaveAnAccountWidget({
-    super.key,
-  });
+  const HaveAnAccountWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -33,8 +30,9 @@ class HaveAnAccountWidget extends StatelessWidget {
                 Navigator.pop(context);
               },
             text: 'تسجيل الدخول',
-            style:
-                TextStyles.semiBold16.copyWith(color: AppColors.primaryColor),
+            style: TextStyles.semiBold16.copyWith(
+              color: AppColors.primaryColor,
+            ),
           ),
         ],
       ),
