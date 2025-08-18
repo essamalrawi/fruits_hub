b4eface1edb47534ed31630955c1e7420554e480
Essam Al-Rawi <essamhamdy198@gmail.com>
2025-08-18 04:09:35 +0300

    Trigger signup cubit
    # Conflicts:
    #	lib/core/services/get_it_service.dart
    #	lib/features/auth/presentation/views/signup_view.dart
    #	pubspec.yaml

--- lib/core/widgets/custom_text_field.dart
+++ lib/core/widgets/custom_text_field.dart
@@ -3,23 +3,32 @@ import 'package:flutter/widgets.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
 
 class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField(
-      {super.key,
-      required this.hintText,
-      required this.textInputType,
-      this.suffixIcon});
+  const CustomTextFormField({
+    super.key,
+    required this.hintText,
+    required this.textInputType,
+    this.suffixIcon,
+    this.onSaved,
+  });
+
   final String hintText;
   final TextInputType textInputType;
   final Widget? suffixIcon;
+  final void Function(String?)? onSaved;
+
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      onSaved: onSaved,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'هذا الحقل مطلوب';
+        }
+      },
       keyboardType: textInputType,
       decoration: InputDecoration(
         suffixIcon: suffixIcon,
-        hintStyle: TextStyles.bold13.copyWith(
-          color: const Color(0xFF949D9E),
-        ),
+        hintStyle: TextStyles.bold13.copyWith(color: const Color(0xFF949D9E)),
         hintText: hintText,
         filled: true,
         fillColor: const Color(0xFFF9FAFA),
@@ -33,10 +42,7 @@ class CustomTextFormField extends StatelessWidget {
   OutlineInputBorder buildBorder() {
     return OutlineInputBorder(
       borderRadius: BorderRadius.circular(4),
-      borderSide: const BorderSide(
-        width: 1,
-        color: Color(0xFFE6E9E9),
-      ),
+      borderSide: const BorderSide(width: 1, color: Color(0xFFE6E9E9)),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_hub/core/helper_functions/on_generate_routes.dart';
+import 'package:fruits_hub/core/services/get_it_service.dart';
 import 'package:fruits_hub/core/services/shared_preferences_singleton.dart';
 import 'package:fruits_hub/core/utils/app_colors.dart';
 import 'package:fruits_hub/firebase_options.dart';
@@ -14,6 +15,7 @@ void main() async {
   await Prefs.init();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  setupGetIt();
   runApp(const FruitHub());
 }
 
